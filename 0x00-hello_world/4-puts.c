45470cd21f71268bd94ab97d9febe13594f3d8ec
Gentle9366 <gentlechukwubuokem9366@gmail.com>
2022-04-07 08:00:33 -0700

    Puts_1

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - entry point
- * 
- * Return: 0 after printing the function
+ * My hello, puts task
+ * Return: 1 after program execution
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
