72831d788e86db73eb4675339b3cd0ba1f20fb30
Gentle9366 <gentlechukwubuokem9366@gmail.com>
2022-04-07 10:19:31 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - prints quote c
+ * Return: 1 if exited properly
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+return (1);
+}
