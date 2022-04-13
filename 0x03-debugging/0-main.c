b5baec27fc7d056c4ee09d4bbaaa9e93e9492ec0
Gentle9366 <gentlechukwubuokem9366@gmail.com>
2022-04-13 16:35:48 -0700

    main

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *main - gives the correct output when given a case of 0
+ *Return: 0 if exited properly
+ */
+int main(void)
+{
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
