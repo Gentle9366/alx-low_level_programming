8c2dba2926e772d835e9aa3488349ff596f7b1ad
Gentle9366 <gentlechukwubuokem9366@gmail.com>
2022-04-20 15:15:12 -0700

    reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *print_rev - prints reversed string, followed by a new line
+ *@s: pointer
+ *Return: 0
+ */
+void print_rev(char *s)
+{
+int = 0
+while (s[i])
+i++;
+while (i--)
+{
+_putchar(s[i]);
+}
+_putchar('\n');
+}
